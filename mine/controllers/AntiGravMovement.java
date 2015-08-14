cfcf93c28300c615bafc0c1c1fc0108a0743742e
James Tautges <jamesdtautges@gmail.com>
2015-08-14 11:54:30 -0500

    Encapsulated forces so it can be extended by other anti-grav systems

--- mine/controllers/AntiGravMovement.java
+++ mine/controllers/AntiGravMovement.java
@@ -39,58 +39,24 @@ public class AntiGravMovement {
 	}
 
 	public void update() {
-		if (me.getX() < 50 || me.getBattleFieldWidth() - me.getX() < 50 || me.getY() < 50 || me.getBattleFieldHeight() - me.getY() < 50) {
-			Point2D relCenter = new Point2D((battleField.getMinX() + battleField.getMaxX()) / 2 - me.getX(), 
-					(battleField.getMinY() + battleField.getMaxY()) / 2 - me.getY());
-			Point2D relEnemy = new Point2D(enemy.getX() - me.getX(), enemy.getY() - me.getY());
-			if (relEnemy.crossProduct(relCenter).getZ() <= 0) {
-				direction = 1;
-			} else {
-				direction = -1;
-			}
-		}
+		getForceToCenter();
 		
 		Point2D force = new Point2D(0, 0);
 
 		// enemy force
-		force = force.subtract(enemyForceFactor * Math.sin(enemy.getAbsoluteBearing()) / Math.pow(enemy.getDistance() - 150, 2),
-				enemyForceFactor * Math.cos(enemy.getAbsoluteBearing()) / Math.pow(enemy.getDistance() - 150, 2));
+		force = addEnemyForce(force);
 
 		// wall force
-
-		force = force.add(wallForceFactor / Math.pow(me.getX(), 3), wallForceFactor / Math.pow(me.getY(), 3));
-		force = force.subtract(wallForceFactor / Math.pow(me.getBattleFieldWidth() - me.getX(), 3),
-				wallForceFactor / Math.pow(me.getBattleFieldHeight() - me.getY(), 3));
+		force = addWallForce(force);
 
 		// corner force
-		for(int i=0; i<4; i++) {
-			force = force.subtract(cornerForceFactor * (corners[i].getX() - me.getX()) / Math.pow(corners[i].distance(me.getX(), me.getY()), 4),
-					cornerForceFactor * (corners[i].getY() - me.getY()) / Math.pow(corners[i].distance(me.getX(), me.getY()), 4));
-		}
+		force = addCornerForce(force);
 
 		// perpendicular force
-		force = force.subtract(-direction * perpendicularForceFactor * Math.cos(enemy.getAbsoluteBearing()) / Math.pow(enemy.getDistance(), 1),
-				direction * perpendicularForceFactor * Math.sin(enemy.getAbsoluteBearing()) / Math.pow(enemy.getDistance(), 1));
+		force = addPerpendicularForce(force);
 
-		// stalling case
-		/*System.out.println("Travelled: " + previousLocation.distance(me.getX(), me.getY()));
-		if(previousLocation.distance(me.getX(), me.getY()) < 1) {
-			direction = -direction;
-		}
-		previousLocation = new Point2D(me.getX(), me.getY());*/
-
-		double angle = Math.atan2(force.getX(), force.getY());
-		
-		if (force.getX() != 0 || force.getY() != 0) {
-			if (Math.abs(Utils.normalRelativeAngle(angle - me.getHeadingRadians())) < Math.PI / 2) {
-				turn = Utils.normalRelativeAngle(angle - me.getHeadingRadians());
-				ahead = Double.POSITIVE_INFINITY;
-			} else {
-				turn = Utils.normalRelativeAngle(angle + Math.PI - me.getHeadingRadians());
-				ahead = Double.NEGATIVE_INFINITY;
-			}
-		}
-
+		// generate movement based on cumulative force
+		generateMovement(force);
 	}
 
 	public void run() {
@@ -102,6 +68,60 @@ public class AntiGravMovement {
 		if(me.getTime() - lastHit > 5) {
 			direction = -direction;
 			lastHit = me.getTime();
+		}
+	}
+	
+	protected void getForceToCenter() {
+		if (me.getX() < 50 || me.getBattleFieldWidth() - me.getX() < 50 || me.getY() < 50 || me.getBattleFieldHeight() - me.getY() < 50) {
+			Point2D relCenter = new Point2D((battleField.getMinX() + battleField.getMaxX()) / 2 - me.getX(), 
+					(battleField.getMinY() + battleField.getMaxY()) / 2 - me.getY());
+			Point2D relEnemy = new Point2D(enemy.getX() - me.getX(), enemy.getY() - me.getY());
+			if (relEnemy.crossProduct(relCenter).getZ() <= 0) {
+				direction = 1;
+			} else {
+				direction = -1;
+			}
+		}
+	}
+	
+	protected Point2D addEnemyForce(Point2D force) {
+		force = force.subtract(enemyForceFactor * Math.sin(enemy.getAbsoluteBearing()) / Math.pow(enemy.getDistance() - 150, 2),
+				enemyForceFactor * Math.cos(enemy.getAbsoluteBearing()) / Math.pow(enemy.getDistance() - 150, 2));
+		return force;
+	}
+	
+	protected Point2D addWallForce(Point2D force) {
+		force = force.add(wallForceFactor / Math.pow(me.getX(), 3), wallForceFactor / Math.pow(me.getY(), 3));
+		force = force.subtract(wallForceFactor / Math.pow(me.getBattleFieldWidth() - me.getX(), 3),
+				wallForceFactor / Math.pow(me.getBattleFieldHeight() - me.getY(), 3));
+		return force;
+	}
+	
+	protected Point2D addCornerForce(Point2D force) {
+		for(int i=0; i<4; i++) {
+			force = force.subtract(cornerForceFactor * (corners[i].getX() - me.getX()) / Math.pow(corners[i].distance(me.getX(), me.getY()), 4),
+					cornerForceFactor * (corners[i].getY() - me.getY()) / Math.pow(corners[i].distance(me.getX(), me.getY()), 4));
+		}
+		return force;
+	}
+	
+	protected Point2D addPerpendicularForce(Point2D force) {
+		force = force.subtract(-direction * perpendicularForceFactor * Math.cos(enemy.getAbsoluteBearing()) / Math.pow(enemy.getDistance(), 1),
+				direction * perpendicularForceFactor * Math.sin(enemy.getAbsoluteBearing()) / Math.pow(enemy.getDistance(), 1));
+		return force;
+	}
+	
+	protected void generateMovement(Point2D force) {
+		double angle = Math.atan2(force.getX(), force.getY());
+		
+		if (force.getX() != 0 || force.getY() != 0) {
+			if (Math.abs(Utils.normalRelativeAngle(angle - me.getHeadingRadians())) < Math.PI / 2) {
+				turn = Utils.normalRelativeAngle(angle - me.getHeadingRadians());
+				ahead = Double.POSITIVE_INFINITY;
+			} else {
+				turn = Utils.normalRelativeAngle(angle + Math.PI - me.getHeadingRadians());
+				ahead = Double.NEGATIVE_INFINITY;
+			}
 		}
 	}
 
